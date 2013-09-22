7b566160d32a43be714786892fbed878fa4ea108
Alin Besnea <alin@2o.com>
2013-09-22 18:59:24 +0300

    Refactoring to simplify possible changes.

--- algo-1/union-find/percolation/src/Percolation.java
+++ algo-1/union-find/percolation/src/Percolation.java
@@ -1,28 +1,22 @@
 public class Percolation 
 {
-	private WeightedQuickUnionUF _unionQuickFind;
-	private Boolean[] _openSites;
 	private int _gridLength;
 	private int _upperVirtualIndex;
 	private int _lowerVirtualIndex;
+	
+	private WeightedQuickUnionUF _unionQuickFind;
+	private Boolean[] _openSites;
 	
 	/**
 	 * Create N-by-N grid, with all sites blocked
 	 */
 	public Percolation(int N)
 	{
-		_unionQuickFind = new WeightedQuickUnionUF(N * N + 2);
 		_gridLength = N;
-		_openSites = new Boolean[N * N + 2];
-		_openSites[0] = true;
-		_openSites[N * N + 1] = true;
-		for (int i = 1; i < N * N + 1; i++)
-		{
-			_openSites[i] = false;
-		}
+		_unionQuickFind = new WeightedQuickUnionUF(_gridLength * _gridLength + 2);
 		
-		_upperVirtualIndex = 0;
-		_lowerVirtualIndex = N * N + 1;
+		initVirtualIndices();
+		initOpenSites();
 	}
 	
 	/**
@@ -30,7 +24,7 @@ public class Percolation
 	 */
 	public void open(int i, int j)
 	{
-		int openValueIndex = 1 + ((i - 1) * _gridLength + (j - 1));
+		int openValueIndex = convertToArrayIndex(i, j);
 		_openSites[openValueIndex] = true;
 		
 		connectRow(openValueIndex, i - 1, j);
@@ -40,13 +34,94 @@ public class Percolation
 		connectColumn(openValueIndex, i, j + 1);
 	}
 	
+	/**
+	 * Is site (row i, column j) open?
+	 */
+	public boolean isOpen(int i, int j)
+	{
+		int arrayIndex = convertToArrayIndex(i, j);
+		return _openSites[arrayIndex];
+	}
+	
+	/**
+	 * Is site (row i, column j) full?
+	 */
+	public boolean isFull(int i, int j)
+	{
+		int arrayIndex = convertToArrayIndex(i, j);
+		return isOpen(i, j) && _unionQuickFind.connected(_upperVirtualIndex, arrayIndex);
+	}
+	
+	/**
+	 * Does the system percolate?
+	 */
+	public boolean percolates()
+	{
+		return _unionQuickFind.connected(_upperVirtualIndex, _lowerVirtualIndex);
+	}
+	
+	@Override
+	public String toString() 
+	{
+		String s = "";
+		
+		//s += "Upper: " + (_openSites[0] ? "1" : "0") + "\n";
+		
+		for (int i = 1; i <= _gridLength; i++)
+		{
+			for (int j = 1; j <= _gridLength; j++)
+			{
+				s += (_openSites[(i - 1) * _gridLength + j] ? "1" : "0") + " ";
+			}
+			
+			s += "\n";
+		}
+		
+		//s += "Lower: " + (_openSites[_gridLength * _gridLength + 1] ? "1" : "0");
+		
+		return s;
+	}
+	
+	private void initVirtualIndices()
+	{
+		_upperVirtualIndex = 0;
+		_lowerVirtualIndex = _gridLength * _gridLength + 1;
+	}
+	
+	/**
+	 * @private
+	 */
+	private void initOpenSites() 
+	{
+		_openSites = new Boolean[_gridLength * _gridLength + 2];
+		
+		_openSites[_upperVirtualIndex] = true;
+		_openSites[_lowerVirtualIndex] = true;
+		
+		for (int i = 1; i < _lowerVirtualIndex; i++)
+		{
+			_openSites[i] = false;
+		}
+	}
+	
+	/**
+	 * @private
+	 */
+	private int convertToArrayIndex(int i, int j)
+	{
+		return 1 + ((i - 1) * _gridLength + (j - 1));
+	}
+	
+	/**
+	 * @private
+	 */
 	private void connectRow(int openValueIndex, int i, int j)
 	{
 		int rowIndex;
 		
 		if (i > 0 && i <= _gridLength)
 		{
-			rowIndex = 1 + ((i - 1) * _gridLength + (j - 1));
+			rowIndex = convertToArrayIndex(i, j);
 			if (isOpen(i, j))
 			{
 				_unionQuickFind.union(rowIndex, openValueIndex);
@@ -66,59 +141,14 @@ public class Percolation
 		}
 	}
 	
+	/**
+	 * @private
+	 */
 	private void connectColumn(int openValueIndex, int i, int j)
 	{
 		if (j > 0 && j <= _gridLength && isOpen(i, j))
 		{
-			_unionQuickFind.union((int) 1 + ((i - 1) * _gridLength + (j - 1)), openValueIndex);
+			_unionQuickFind.union(convertToArrayIndex(i, j), openValueIndex);
 		}
-	}
-	
-	/**
-	 * Is site (row i, column j) open?
-	 */
-	public boolean isOpen(int i, int j)
-	{
-		int arrayIndex = 1 + ((i - 1) * _gridLength + (j - 1));
-		return _openSites[arrayIndex];
-	}
-	
-	/**
-	 * Is site (row i, column j) full?
-	 */
-	public boolean isFull(int i, int j)
-	{
-		int arrayIndex = 1 + ((i - 1) * _gridLength + (j - 1));
-		return _openSites[arrayIndex];
-	}
-	
-	/**
-	 * Does the system percolate?
-	 */
-	public boolean percolates()
-	{
-		return _unionQuickFind.connected(_upperVirtualIndex, _lowerVirtualIndex);
-	}
-	
-	@Override
-	public String toString() 
-	{
-		String s = "";
-		
-		s += "Upper: " + (_openSites[0] ? "1" : "0") + "\n";
-		
-		for (int i = 1; i <= _gridLength; i++)
-		{
-			for (int j = 1; j <= _gridLength; j++)
-			{
-				s += (_openSites[(i - 1) * _gridLength + j] ? "1" : "0") + " ";
-			}
-			
-			s += "\n";
-		}
-		
-		s += "Lower: " + (_openSites[_gridLength * _gridLength + 1] ? "1" : "0");
-		
-		return s;
 	}
 }
