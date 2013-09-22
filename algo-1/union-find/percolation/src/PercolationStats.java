d7b86bb8961e4c84aaf7f56d6fff8fe53d38cf27
Alin Besnea <alin@2o.com>
2013-09-22 18:30:32 +0300

    Added the Percolation call and one random run of the percolation.

--- algo-1/union-find/percolation/src/PercolationStats.java
+++ algo-1/union-find/percolation/src/PercolationStats.java
@@ -44,6 +44,47 @@ public class PercolationStats
 	 */
 	public static void main(String[] args) 
 	{
-
+		//System.out.println("Hello, world!");
+		int N = 5;
+		
+		Percolation percolation = new Percolation(N);
+		int randomRow;
+		int randomColumn;
+		do
+		{
+			randomRow = StdRandom.uniform(1, N + 1);
+			randomColumn = StdRandom.uniform(1, N + 1);
+			
+			//percolation.open(randomRow, randomColumn);
+			tryPercolate(percolation, randomRow, randomColumn);
+		}
+		while (!percolation.percolates());
+		
+		/*
+		tryPercolate(percolation, 1, 1);
+		tryPercolate(percolation, 1, 2);
+		tryPercolate(percolation, 1, 3);
+		
+		tryPercolate(percolation, 3, 1);
+		tryPercolate(percolation, 3, 2);
+		tryPercolate(percolation, 3, 3);
+		
+		tryPercolate(percolation, 2, 3);
+		
+		tryPercolate(percolation, 5, 1);
+		tryPercolate(percolation, 5, 2);
+		tryPercolate(percolation, 5, 3);
+		
+		tryPercolate(percolation, 4, 3);
+		*/
+	}
+	
+	public static void tryPercolate(Percolation percolation, int i, int j)
+	{
+		percolation.open(i, j);
+		System.out.println("open: " + i + "," + j);
+		System.out.println(percolation.toString());
+		System.out.println("percolates: " + percolation.percolates());
+		System.out.println("\n");
 	}
 }
