1200c9c5f31b245c24ebe23aed41a00b4d4ffd28
Alin Besnea <alin@2o.com>
2013-09-22 14:52:10 +0300

    Initial version of the Percolation class. Contains only the interface of the class.

---
+++ algo-1/union-find/percolation/src/Percolation.java
@@ -0,0 +1,42 @@
+public class Percolation 
+{
+	/**
+	 * Create N-by-N grid, with all sites blocked
+	 */
+	public Percolation(int N)
+	{
+	
+	}
+	
+	/**
+	 * Open site (row i, column j) if it is not already
+	 */
+	public void open(int i, int j) 
+	{
+	
+	}
+	
+	/**
+	 * Is site (row i, column j) open?
+	 */
+	public boolean isOpen(int i, int j)
+	{
+		return false;
+	}
+	
+	/**
+	 * Is site (row i, column j) full?
+	 */
+	public boolean isFull(int i, int j)
+	{
+		return false;
+	}
+	
+	/**
+	 * Does the system percolate?
+	 */
+	public boolean percolates()
+	{
+		return false;
+	}
+}
