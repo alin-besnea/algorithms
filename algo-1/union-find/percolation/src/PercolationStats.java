0c52e934abe0fa1544e10b50f4dc44feb404e764
Alin Besnea <alin@2o.com>
2013-09-22 14:52:37 +0300

    Initial commit of the entry point and the class that will show the stats of the percolations.

---
+++ algo-1/union-find/percolation/src/PercolationStats.java
@@ -0,0 +1,49 @@
+public class PercolationStats 
+{
+	/**
+	 * Perform T independent computational experiments on an N-by-N grid
+	 */
+	public PercolationStats(int N, int T)
+	{
+		
+	}
+	
+	/**
+	 * Perform T independent computational experiments on an N-by-N grid
+	 */
+	public double mean()
+	{
+		return 0.0d;
+	}
+	
+	/**
+	 * Sample standard deviation of percolation threshold
+	 */
+	public double stddev()
+	{
+		return 0.0d;
+	}
+	
+	/**
+	 * Returns lower bound of the 95% confidence interval
+	 */
+	public double confidenceLo()
+	{
+		return 0.0d;
+	}
+	
+	/**
+	 * Returns upper bound of the 95% confidence interval
+	 */
+	public double confidenceHi()
+	{
+		return 0.0d;
+	}
+	/**
+	 * Entry point. Takes two arguments: 
+	 */
+	public static void main(String[] args) 
+	{
+
+	}
+}
